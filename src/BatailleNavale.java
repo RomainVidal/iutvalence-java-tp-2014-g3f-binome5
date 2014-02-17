40a2948db17b6d1843e2e98e40186088d7f2c437
vidalr <vidalr@PC-B102-05.iut-valence.fr>
2014-02-17 16:39:07 +0100

    Création de la classe Bataille Navale.
    Cette classe représente une partie de Bataille Navale.

---
+++ src/BatailleNavale.java
@@ -0,0 +1,14 @@
+/**
+ * Représente une partie de Bataille Navale.
+ */
+public class BatailleNavale {
+
+	/**
+	 * permet de jouer une partie de  Bataille Navale.
+	 */
+	public void jouer() {
+		
+		
+	}
+
+}
