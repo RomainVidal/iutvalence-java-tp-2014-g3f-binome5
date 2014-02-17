670d4dfdcb37609917fae40972a07be8b32bcff7
vidalr <vidalr@PC-B102-05.iut-valence.fr>
2014-02-17 16:36:42 +0100

    Création du lanceur de partie.
    Méthode main : lance une nouvelle partie de Bataille Navale.

--- src/LanceurDeBatailleNavale.java
+++ src/LanceurDeBatailleNavale.java
@@ -2,7 +2,16 @@
 public class LanceurDeBatailleNavale {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Création d'une partie de Bataille Navale");
+		
+		BatailleNavale partieBatailleNavale = new BatailleNavale();
+		
+		System.out.println("Démarrage de la partie");
+		
+		partieBatailleNavale.jouer();
+		
+		System.out.println("Fin de la partie");	
 
 	}
 
