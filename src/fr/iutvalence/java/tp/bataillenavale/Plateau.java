957b4fdcd795b966ce1e9e9bd136000d49938aa7
vidalr <vidalr@PC-B102-06.iut-valence.fr>
2014-02-24 17:02:38 +0100

    Création d'une nouvelle classe Plateau.
    On créé un nouvelle classe Plateau qui fera appel à la classe Grille
    pour créer le plateau de jeu.

--- src/fr/iutvalence/java/tp/bataillenavale/Plateau.java
+++ src/fr/iutvalence/java/tp/bataillenavale/Plateau.java
@@ -1,31 +1,10 @@
 package fr.iutvalence.java.tp.bataillenavale;
 
 /**
- * Plateau de jeu comportant deux grilles identiques (une pour chaque joueur),
+ * Grille de jeu comportant deux grilles identiques (une pour chaque joueur),
  * cachées du joueur adverse par un cache placé au milieu.
  */
+
 public class Plateau {
 
-	/**
-	 * Constante représentant le nombre de lignes d'une grille.
-	 */
-	private static final int NOMBRE_DE_CASES_X = 10;
-	/**
-	 * Constante représentant le nombre de colonnes d'une grille.
-	 */
-	private static final int NOMBRE_DE_CASES_Y = 10;
-
-	/**
-	 * Un tableau de cases en deux dimensions représentant chacune des cases
-	 * d'une grille de jeu.
-	 */
-	private Case[][] cases;
-
-	public Plateau() {
-		cases = new Case[NOMBRE_DE_CASES_X][NOMBRE_DE_CASES_Y];
-		for (int i = 0; i < NOMBRE_DE_CASES_X; i++) {
-			for (int j = 0; j < NOMBRE_DE_CASES_Y; j++)
-				cases[i][j] = new Case(i + 1, j + 1);
-		}
-	}
 }
