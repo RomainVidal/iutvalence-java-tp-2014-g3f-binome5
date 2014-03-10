428fd32dfce0fdf208793144284c5a52ccee85d4
vidalr <vidalr@PC-B102-04.iut-valence.fr>
2014-03-10 16:40:25 +0100

    Création d'une énumération.
    Définit l'orientation du bateau sur la grille.

---
+++ src/fr/iutvalence/java/tp/bataillenavale/Orientation.java
@@ -0,0 +1,19 @@
+package fr.iutvalence.java.tp.bataillenavale;
+
+/**
+ * Orientation du bateau sur la grille.
+ *
+ */
+public enum Orientation {
+
+	/**
+	 * Le bateau est placé horizontalement.
+	 */
+	Horizontal,
+	
+	/**
+	 * Le bateau est placé verticalement.
+	 */
+	Vertical;
+	
+}
