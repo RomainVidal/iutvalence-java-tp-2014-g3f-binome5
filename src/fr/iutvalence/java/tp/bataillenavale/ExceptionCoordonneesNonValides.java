3521eef882dd1d65c7fc04067035cddbdf1b5dc5
vidalr <vidalr@PC-B102-04.iut-valence.fr>
2014-03-10 16:39:39 +0100

    Création d'une exception.
    Exception retournant une erreur si les coordonnées de la case
    n'appartiennent pas à la grille.

---
+++ src/fr/iutvalence/java/tp/bataillenavale/ExceptionCoordonneesNonValides.java
@@ -0,0 +1,10 @@
+package fr.iutvalence.java.tp.bataillenavale;
+
+/**
+ * Les coordonnées entrées en paramètre indiquent une case n'appartenant pas à
+ * la grille.
+ * 
+ */
+public class ExceptionCoordonneesNonValides extends Exception {
+
+}
