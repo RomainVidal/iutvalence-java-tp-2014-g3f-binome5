7f0aa9bb1e53fcaf000526ecb7b0e7fcd7623878
vidalr <vidalr@PC-B102-04.iut-valence.fr>
2014-03-10 16:31:47 +0100

    Création de la classe abstraite Bateau.
    On a créé une classe Bateau qui nous permettra de définir les différents
    types de bateaux à placer sur la grille.

---
+++ src/fr/iutvalence/java/tp/bataillenavale/Bateau.java
@@ -0,0 +1,28 @@
+package fr.iutvalence.java.tp.bataillenavale;
+
+/**
+ * Classe abstraite permettant de donner un type aux bateaux qui seront définis
+ * par la suite.
+ * 
+ */
+public abstract class Bateau {
+
+	/**
+	 * Taille du bateau (nombre de cases qu'il occupe sur la grille).
+	 */
+	private int taille;
+	
+	/**
+	 * Orientation du bateau.
+	 */
+	private Orientation orientation;
+	
+	/**
+	 * Position de la première case du bateau.
+	 */
+	private Position positionPremiereCase;
+	
+	
+
+
+}
